277ce7f84d064ff8cd9138e757d2a10d177baccf
initcyber <justin@initcyber.com>
2024-01-31 22:11:32 +0000

    repush

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-OWNER=$OWNER
-REPOSITORY=$REPOSITORY
-TOKEN=$TOKEN
+GH_OWNER=$GH_OWNER
+GH_REPOSITORY=$GH_REPOSITORY
+GH_TOKEN=$GH_TOKEN
 
 RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
-RUNNER_NAME="SELFHOSTED-GHA-Docker-${RUNNER_SUFFIX}"
+RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
 
-REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${TOKEN}" https://api.github.com/repos/${OWNER}/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --unattended --url https://github.com/${OWNER}/${REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
+./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
 
 cleanup() {
     echo "Removing runner..."
